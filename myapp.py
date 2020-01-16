ad7c25c8eaf7da1b32026fef54e65bcb1d9342c3
Yeow Jing Jong <yeowjingjong@gmail.com>
2020-01-16 16:57:11 +0800

    added url link to show another route

--- myapp.py
+++ myapp.py
@@ -4,12 +4,14 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    signed_in = True 
+    return render_template('index.html', signed_in=signed_in)
 
 
 @app.route("/contact")
 def contact():
-    return render_template('contact.html')
+    signed_in = True
+    return render_template('contact.html', signed_in=signed_in)
 
 
 if __name__ == '__main__':
